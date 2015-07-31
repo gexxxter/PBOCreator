cba2937b761218c884071bec8824164a95a8aac7
gexxxter <Basti_12@gmx.de>
2015-07-31 20:40:11 +0200

    Added rudimentary download functionality

--- src/main/java/org/slaxxx/FileUploadControler.java
+++ src/main/java/org/slaxxx/FileUploadControler.java
@@ -2,6 +2,11 @@ package org.slaxxx;
 
 import java.io.BufferedOutputStream;
 import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+import javax.servlet.http.HttpServletResponse;
+import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter.DEFAULT;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,18 +24,30 @@ public class FileUploadControler {
 	}
 
 	@RequestMapping(value = "/upload", method = RequestMethod.POST)
-	public @ResponseBody String handleFileUpload(
-			@RequestParam("name") String name,
+	public @ResponseBody String handleFileUpload(HttpServletResponse response,
 			@RequestParam("file") MultipartFile file) {
 		if (!file.isEmpty()) {
-			try (FileOutputStream stream = new FileOutputStream(name)) {
+			try (FileOutputStream stream = new FileOutputStream(
+					file.getOriginalFilename())) {
 				byte[] bytes = file.getBytes();
 				stream.write(bytes);
+				//Write to hdd done
+				startDownload(response, bytes);
 			} catch (Exception ex) {
 				return "Something went horribly wrong :(!";
 			}
 		}
-		return "File uploaded";
+		return "File " + file.getOriginalFilename() + " uploaded successful";
+	}
+
+	private void startDownload(HttpServletResponse response, byte[] bytes)
+			throws IOException {
+		response.reset();
+		response.setContentType("application/octet-stream");
+		response.setContentLength(bytes.length);
+		
+		OutputStream outStream = response.getOutputStream();
+		outStream.write(bytes);
 	}
 
 }
