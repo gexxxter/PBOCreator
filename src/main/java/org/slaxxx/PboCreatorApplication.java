87d058332b58394bf83a27acb2516273d0106e4c
gexxxter <Basti_12@gmx.de>
2015-07-31 19:42:30 +0200

    corrected shitty package shit

---
+++ src/main/java/org/slaxxx/PboCreatorApplication.java
@@ -0,0 +1,12 @@
+package org.slaxxx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PboCreatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PboCreatorApplication.class, args);
+    }
+}
