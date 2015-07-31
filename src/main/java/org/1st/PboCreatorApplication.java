bdc43863bf077813e9890af0ddf768ca8c7c7ac9
gexxxter <Basti_12@gmx.de>
2015-07-31 19:36:17 +0200

    Added basic spring.io dependencies and embedded tomcat

---
+++ src/main/java/org/1st/PboCreatorApplication.java
@@ -0,0 +1,12 @@
+package org.1st;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PboCreatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PboCreatorApplication.class, args);
+    }
+}
